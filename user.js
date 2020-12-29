196534104660ee4e6c0eb33bf7ba74e59a796157
Ricky <rickdhanota@gmail.com>
2020-12-29 00:17:39 -0800

    fixed the other user stuff

--- user.js
+++ user.js
@@ -16,12 +16,27 @@ class User{
     displayBalance(){
         console.log(`${this.username} account balance is: ${this.accountBalance}`);
     }
+
+    transferMoney(otherUser, amount){
+        if(this.accountBalance < amount){
+            console.log("Not enough funds");
+        }
+        else{
+            this.accountBalance -= amount;
+            otherUser.accountBalance += amount;
+        }
+    }
 }
 
 const ricky = new User("Ricky Dhanota", "rickdhanota@gmail.com");
+const sukhpreet = new User("Sukhpreet Dhanota", "preetxgill@gmail.com");
+
 ricky.makeDeposit(200);
 ricky.makeDeposit(100);
 ricky.makeDeposit(300);
 ricky.displayBalance();
 ricky.makeWithdrawal(100);
+ricky.displayBalance();
+ricky.transferMoney(sukhpreet, 100);
+sukhpreet.displayBalance();
 ricky.displayBalance();
