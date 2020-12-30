33b8d35940bb82061057520c5969c2b53f29eb3d
Ricky <rickdhanota@gmail.com>
2020-12-29 23:29:28 -0800

    adding another class

--- user.js
+++ user.js
@@ -34,6 +34,25 @@ class User{
     }
 }
 
+class BankAccount{
+    constructor(intRate, balance) { // don't forget to add some default values for these parameters!
+    // your code here! (remember, this is where we specify the attributes for our class)
+	// don't worry about user info here; we'll involve the User class soon
+    }
+    deposit(amount) {
+	// your code here
+    }
+    withdraw(amount) {
+	// your code here
+    }
+    displayAccountInfo() {
+	// your code here
+    }
+    yieldInterest() {
+	// your code here
+    }
+}
+
 const ricky = new User("Ricky Dhanota", "rickdhanota@gmail.com");
 const sukhpreet = new User("Sukhpreet Dhanota", "preetxgill@gmail.com");
 
